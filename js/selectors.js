0e8c795b8dcdda96e2393c2d8d6afb348020b8ee
naizal Edy <pritiadhikari@gmail.com>
2018-10-02 04:12:13 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+function paragraphSelector()
+{
+  $('p')
+}
+
+function lastImageSelector()
+{
+  
+  $('div:last')
+  
+}
+
+function ninjaBabySelector()
+{
+  $('#baby-ninja')
+}
+
+function divSelector ()
+{
+  $('.pics')
+}
+
+function firstListItem()
+{
+  $('#pic-list ul:first-child')
+}
